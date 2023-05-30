66c8b74b0e77a5120d57908d45b2ecc940abece9
CF1079 <cjfalk7@gmail.com>
2023-05-30 10:44:15 +1000

    update

---
+++ Updated Version OO Design /Population.py
@@ -0,0 +1,14 @@
+## Population.py 
+## __author__ = Charlie Falk 
+## Date Crated = 11/06/2023
+## Date Updated = 11/06/2023
+
+
+
+
+
+
+""" This version of code has been updated from the specialist maths 2021 IA1 Assignment
+    It is still implementing a leslie matrix based approach, however, the key difference 
+    is the code is written in an object oriented formated, ultimatley making the code more 
+    efficient and easier to use. """
